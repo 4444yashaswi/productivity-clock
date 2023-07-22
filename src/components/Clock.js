a1ac0eff7ecaa33f1b81fa3d5cd41313cf43b859
4444yashaswi <4444yashaswi@gmail.com>
2023-07-23 03:05:59 +0530

    Initial timer value update

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const Clock = ({ isStart, isReset }) => {
-  const [timer, setTimer] = useState(35990);
+  const [timer, setTimer] = useState(0);
 
   // Side Effects
   useEffect(() => {
